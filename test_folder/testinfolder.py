f19f8c5efe103fb7d2f088faa3cc8d8af35483ae
ChatDDZZ2203 <125794957+ChatDDZZ2203@users.noreply.github.com>
2023-07-17 19:24:10 +0300

    Update testinfolder.py

--- test_folder/testinfolder.py
+++ test_folder/testinfolder.py
@@ -1,7 +1,5 @@
 print("I`m living in the test_folder")
 
-print('What. If it works...')
-
 print('Looks like it doesnt. Or maybe... Hm')
 
 print('I may need to remember that threading.Thread doesnt have raise_exception method')
