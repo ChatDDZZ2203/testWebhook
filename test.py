c9c9933f3e0c4a1f9d4ff9b184ed2634b9d18a8b
ChatDDZZ2203 <125794957+ChatDDZZ2203@users.noreply.github.com>
2023-07-17 11:19:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,6 @@ print("Is this going to be the cause of the POST request?")
 print("Trying again. Added methods=['POST'] to the listener")
 
 print("Add some beautiful printing of request`s data")
+
+print("Fix it a bit")
+
