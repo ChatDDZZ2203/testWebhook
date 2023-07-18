7430fb1b873432a520b298a80c9d2180d02952d5
ChatDDZZ2203 <dandy.d22.z03@gmail.com>
2023-07-18 23:34:40 +0300

    Test commit

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function changeWithDelay(delayMS, element) {
   await sleep(delayMS);
-  element.innerText = `This element was changed after ${delayMS / 1000} seconds`;
+  element.innerText = `This element was changed after ${delayMS / 1000} seconds passed from the program start`;
 }
 
 document.querySelectorAll('.manipulative').forEach((element) => {
